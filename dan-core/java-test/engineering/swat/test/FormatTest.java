567e3006bf48e8a3e2b0987232db40b2158bb8ea
pinoval <pinoval@gmail.com>
2018-07-03 11:49:44 +0200

    Added tests and corresponding project structure

---
+++ dan-core/java-test/engineering/swat/test/FormatTest.java
@@ -0,0 +1,32 @@
+package engineering.swat.test;
+
+import static io.parsingdata.metal.util.EncodingFactory.enc;
+import static io.parsingdata.metal.util.ParseStateFactory.stream;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.runners.Parameterized;
+
+import engineering.swat.formats.SimpleJPEG;
+import engineering.swat.formats.SimplePNG;
+import io.parsingdata.metal.data.ParseState;
+import io.parsingdata.metal.util.ParameterizedParse;
+
+public class FormatTest extends ParameterizedParse {
+
+    @Parameterized.Parameters(name="{0} ({4})")
+    public static Collection<Object[]> data() throws URISyntaxException, IOException {
+        return Arrays.asList(new Object[][] {
+            { "PNG", SimplePNG.PNG_389_113_24_28_0_1, parseState("/test.png"), enc(), true },
+            { "JPEG", SimpleJPEG.Format_611_51_44_48_0_1 , parseState("/test.jpg"), enc(), true }
+        });
+    }
+
+    private static ParseState parseState(final String path) throws URISyntaxException, IOException {
+        return stream(FormatTest.class.getResource(path).toURI());
+    }
+
+}
