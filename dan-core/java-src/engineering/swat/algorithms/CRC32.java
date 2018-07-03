c16c54175ca2d2979fe8abac41ac32a3abed08f8
pinoval <pinoval@gmail.com>
2018-07-03 11:48:53 +0200

    Added interface for CRC32 algorithm (for testing external functions)

---
+++ dan-core/java-src/engineering/swat/algorithms/CRC32.java
@@ -0,0 +1,11 @@
+package engineering.swat.algorithms;
+
+import io.parsingdata.metal.expression.value.ValueExpression;
+
+public class CRC32{
+
+	public ValueExpression apply(ValueExpression... vs) {
+		return vs[0];
+		
+	}
+}
