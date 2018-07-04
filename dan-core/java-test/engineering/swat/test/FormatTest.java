ef77385aea4cc7f6a8245cae62266de313849aff
pinoval <pinoval@gmail.com>
2018-07-04 14:17:20 +0200

    Test is now generic and configurable via JSON file, and it prints the resulting parse graphs

--- dan-core/java-test/engineering/swat/test/FormatTest.java
+++ dan-core/java-test/engineering/swat/test/FormatTest.java
@@ -4,28 +4,54 @@ import static io.parsingdata.metal.util.EncodingFactory.enc;
 import static io.parsingdata.metal.util.ParseStateFactory.stream;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.net.MalformedURLException;
 import java.net.URISyntaxException;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 import org.junit.runners.Parameterized;
 
-import engineering.swat.formats.SimpleJPEG;
-import engineering.swat.formats.SimplePNG;
 import io.parsingdata.metal.data.ParseState;
-import io.parsingdata.metal.util.ParameterizedParse;
+import io.parsingdata.metal.token.Token;
 
 public class FormatTest extends ParameterizedParse {
+	
+	public static final String CONFIG_FILE_PATH = "/test-properties.json";
 
-    @Parameterized.Parameters(name="{0} ({4})")
+    @Parameterized.Parameters(name="{0} ({5})")
     public static Collection<Object[]> data() throws URISyntaxException, IOException {
-        return Arrays.asList(new Object[][] {
-            { "PNG", SimplePNG.PNG, parseState("/test.png"), enc(), true },
-            { "JPEG", SimpleJPEG.Format, parseState("/test.jpg"), enc(), true }
-        });
+    	return json2list(parseProperties(CONFIG_FILE_PATH));
+    }
+    
+    private static JSONObject parseProperties(final String path) throws MalformedURLException, IOException, URISyntaxException {
+    	JSONTokener tokener = new JSONTokener(FormatTest.class.getResource(path).toURI().toURL().openStream());
+    	return new JSONObject(tokener);
+    }
+    
+    private static Collection<Object[]> json2list(JSONObject json) throws URISyntaxException, IOException{
+    	List<Object[]> list = new ArrayList<>();
+    	for (String format: json.keySet()) {
+    		for (int i = 0; i < json.getJSONArray(format).length(); i++) {
+    			String[] formatAndTokenStart = format.split("\\.");
+    			try {
+					Class clz = Class.forName("engineering.swat.formats." + formatAndTokenStart[0]);
+					Field f = clz.getField(formatAndTokenStart[1]);
+					String fileName = json.getJSONArray(format).getString(i);
+					list.add(new Object[]{ format + Integer.toString(i), (Token) f.get(null), parseState(formatAndTokenStart[0], "/" + fileName), enc(), true,  formatAndTokenStart[0], fileName});
+				} catch (ClassNotFoundException | SecurityException  | NoSuchFieldException | IllegalArgumentException | IllegalAccessException | JSONException e) {
+					e.printStackTrace();
+				} 
+    		}
+    	}
+    	return list;
     }
 
-    private static ParseState parseState(final String path) throws URISyntaxException, IOException {
+    private static ParseState parseState(final String format, final String path) throws URISyntaxException, IOException {
         return stream(FormatTest.class.getResource(path).toURI());
     }
 
---
+++ dan-core/java-test/engineering/swat/test/ParameterizedParse.java
@@ -0,0 +1,42 @@
+package engineering.swat.test;
+
+import static io.parsingdata.metal.util.EnvironmentFactory.env;
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+
+import io.parsingdata.metal.data.ParseState;
+import io.parsingdata.metal.encoding.Encoding;
+import io.parsingdata.metal.token.Token;
+
+@Ignore
+@RunWith(Parameterized.class)
+public class ParameterizedParse {
+
+    @Parameter public String description;
+    @Parameter(1) public Token token;
+    @Parameter(2) public ParseState parseState;
+    @Parameter(3) public Encoding encoding;
+    @Parameter(4) public boolean result;
+    @Parameter(5) public String format;
+    @Parameter(6) public String fileName;
+
+    @Test
+    public void test() throws IOException {
+    	Optional<ParseState> graph = token.parse(env(parseState, encoding));
+        System.out.println("Result of parsing "+ fileName+ " with "+ format+ " specification:");
+        System.out.println();
+        System.out.println(graph.toString());
+        System.out.println("====================================================================================================");
+        System.out.println();
+    	assertEquals(result, graph.isPresent());
+    }
+
+}
